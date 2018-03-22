e5365e5c56732c0e9a953c458e8b591bdd6e6802
wangqi <wang.qi@qikuyx.com>
2018-03-22 20:15:21 +0800

    Modify version

--- src/main/java/jp/coolfactory/data/Version.java
+++ src/main/java/jp/coolfactory/data/Version.java
@@ -7,8 +7,8 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class Version {
 
-    public static final String VERSION = "master.53.063f8c0";
-    public static final String BUILD_DATE = "2018-03-15 15:23:56:056 CST";
+    public static final String VERSION = "master.55.0f0654e";
+    public static final String BUILD_DATE = "2018-03-20 18:05:16:016 CST";
 
     public static final ConcurrentHashMap<String, Object> CONTEXT = new ConcurrentHashMap<>();
 
